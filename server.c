a68e928aa6aab6d01f1096d409d30a1c994ed4ab
BazilSuhail <bazil1854@gmail.com>
2024-09-24 21:33:35 +0500

    Added Updating and Deleting a File Functionality !!

--- server.c
+++ server.c
@@ -12,7 +12,7 @@
 #include <errno.h>
 
 #define MAX_SIZE 1024
-#define MAX_STORAGE 50
+#define MAX_STORAGE 50*1024
 
 #define MAX_FILES 100
 #define MAX_FILENAME_SIZE 100
@@ -545,6 +545,175 @@ int parseFileAfterAsterisk(const char *userName, char fileNames[MAX_FILES][MAX_F
 }
 
 /* =====================================================================
+            Delete file req from user
+========================================================================  */
+
+void delete_File_from_user_Config(int clientSocket, const char *userName, const char *fileName)
+{
+    char filePath[1024];
+    snprintf(filePath, sizeof(filePath), "%s/%s.config", userName, userName);
+
+    char userFilePath[1024];
+    snprintf(userFilePath, sizeof(userFilePath), "%s/%s", userName, fileName);
+
+    if (access(userFilePath, F_OK) != 0)
+    {
+        const char *fileNotFoundMsg = "File not found.";
+        send(clientSocket, fileNotFoundMsg, strlen(fileNotFoundMsg), 0);
+        printf("File '%s' not found for user: %s\n", fileName, userName);
+        return;
+    }
+
+    const char *fileFoundMsg = "File is found!!";
+    send(clientSocket, fileFoundMsg, strlen(fileFoundMsg), 0);
+    printf("File '%s' found for user: %s\n", fileName, userName);
+
+    FILE *configFile = fopen(filePath, "r");
+    if (!configFile)
+    {
+        perror("Error opening config file");
+        const char *errorMsg = "Error opening config file.";
+        send(clientSocket, errorMsg, strlen(errorMsg), 0);
+        return;
+    }
+
+    char password[256];
+    char line[1024];
+    char fileNames[MAX_FILES][MAX_FILENAME_SIZE];
+    int fileSizes[MAX_FILES];
+    int fileCount = 0;
+    int fileIndex = -1;
+
+    if (fgets(password, sizeof(password), configFile) == NULL)
+    {
+        perror("Error reading password from config file");
+        fclose(configFile);
+        const char *errorMsg = "Error reading config file.";
+        send(clientSocket, errorMsg, strlen(errorMsg), 0);
+        return;
+    }
+
+    if (fgets(line, sizeof(line), configFile) == NULL || strcmp(line, "*\n") != 0)
+    {
+        perror("Error reading asterisk line from config file");
+        fclose(configFile);
+        const char *errorMsg = "Error reading config file.";
+        send(clientSocket, errorMsg, strlen(errorMsg), 0);
+        return;
+    }
+
+    while (fgets(line, sizeof(line), configFile) != NULL)
+    {
+        char tempFileName[MAX_FILENAME_SIZE];
+        int tempFileSize;
+
+        if (sscanf(line, "%s - %d", tempFileName, &tempFileSize) == 2)
+        {
+            strcpy(fileNames[fileCount], tempFileName);
+            fileSizes[fileCount] = tempFileSize;
+
+            if (strcmp(tempFileName, fileName) == 0)
+            {
+                fileIndex = fileCount;
+            }
+
+            fileCount++;
+        }
+    }
+
+    fclose(configFile);
+
+    if (fileIndex == -1)
+    {
+        const char *fileNotFoundInConfig = "File not found in config.";
+        send(clientSocket, fileNotFoundInConfig, strlen(fileNotFoundInConfig), 0);
+        printf("File '%s' not found in config for user: %s\n", fileName, userName);
+        return;
+    }
+
+    if (remove(userFilePath) != 0)
+    {
+        perror("Error deleting file from user's folder");
+        const char *deleteError = "Error deleting file.";
+        send(clientSocket, deleteError, strlen(deleteError), 0);
+        return;
+    }
+
+    FILE *configFileWrite = fopen(filePath, "w");
+    if (!configFileWrite)
+    {
+        perror("Error opening config file for writing");
+        const char *errorMsg = "Error updating config file.";
+        send(clientSocket, errorMsg, strlen(errorMsg), 0);
+        return;
+    }
+
+    fprintf(configFileWrite, "%s", password);
+    fprintf(configFileWrite, "*\n");
+
+    for (int i = 0; i < fileCount; i++)
+    {
+        if (i != fileIndex)
+        {
+            fprintf(configFileWrite, "%s - %d\n", fileNames[i], fileSizes[i]);
+        }
+    }
+
+    fclose(configFileWrite);
+
+    const char *successMsg = "File successfully deleted.";
+    send(clientSocket, successMsg, strlen(successMsg), 0);
+    printf("File '%s' successfully deleted from user: %s\n", fileName, userName);
+}
+
+/* =====================================================================
+                 Update the file requested from user
+========================================================================  */
+void receive_updated_file_content(int clientSocket, const char *userName)
+{
+    char fileName[256];
+    ssize_t fileNameSize = recv(clientSocket, fileName, sizeof(fileName) - 1, 0);
+    if (fileNameSize <= 0)
+    {
+        perror("Error receiving file name");
+        close(clientSocket);
+        return;
+    }
+    fileName[fileNameSize] = '\0';
+
+    printf("Receiving file: %s\n", fileName);
+    char filePath[1024];
+    snprintf(filePath, sizeof(filePath), "%s/%s", userName, fileName);
+
+    FILE *encoded_file = fopen(filePath, "w+");
+    if (encoded_file == NULL)
+    {
+        perror("Error creating or opening file");
+        close(clientSocket);
+        return;
+    }
+    send(clientSocket, "$READY$", 7, 0);
+    char buffer[1024];
+    ssize_t bytesRead;
+    while ((bytesRead = recv(clientSocket, buffer, sizeof(buffer), 0)) > 0)
+    {
+        fwrite(buffer, sizeof(char), bytesRead, encoded_file);
+    }
+
+    if (bytesRead < 0)
+    {
+        perror("Error receiving file data");
+    }
+    else
+    {
+        printf("File received and saved successfully in %s.\n", filePath);
+        send(clientSocket, "$SUCCESS$", 9, 0);
+    }
+    fclose(encoded_file);
+}
+
+
+/* =====================================================================
             handle download and Upload Signal from client
 ========================================================================  */
 
@@ -563,7 +732,7 @@ void processFileManagement(int clientSocket, const char *userName)
     if (option == 1)
     {
         char fileName[MAX_SIZE];
-        size_t fileSize;
+        long fileSize;
 
         bytesReceived = recv(clientSocket, fileName, sizeof(fileName) - 1, 0);
         if (bytesReceived <= 0)
@@ -573,7 +742,8 @@ void processFileManagement(int clientSocket, const char *userName)
         }
         fileName[bytesReceived] = '\0';
 
-        bytesReceived = recv(clientSocket, &fileSize, sizeof(fileSize), 0);
+        bytesReceived = recv(clientSocket, (char*)&fileSize, sizeof(fileSize), 0);
+        
         if (bytesReceived <= 0)
         {
             perror("Error receiving file size");
@@ -616,6 +786,24 @@ void processFileManagement(int clientSocket, const char *userName)
     else if (option == 3)
     {
         viewFile(clientSocket, userName);
+    }
+    else if (option == 4)
+    {
+        char fileName[MAX_FILENAME_SIZE];
+
+        bytesReceived = recv(clientSocket, fileName, sizeof(fileName) - 1, 0);
+        if (bytesReceived <= 0)
+        {
+            perror("Error receiving file name for download");
+            return;
+        }
+        fileName[bytesReceived] = '\0';
+        delete_File_from_user_Config(clientSocket, userName, fileName);
+    }
+
+    else if (option == 5)
+    { 
+        receive_updated_file_content(clientSocket, userName);
     }
 }
 
